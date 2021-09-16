f37bc715888e47d55203f139780f91c0afc3a4bc
Mridul Singh <77195905+zypher9@users.noreply.github.com>
2021-09-16 08:25:17 +0530

    Update upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=zypher9/project4
 
 # Step 2:  
 # Authenticate & tag
+docker login -u zypher9
+docker tag project4 $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
